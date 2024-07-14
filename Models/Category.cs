741b3664bce987127d6b4ba7690fc57904b434c8
ali fil <alifil5333@gmail.com>
2024-07-14 20:42:58 +0300

    category işlemleri bitmiştir

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace E_Ticaret.Models
 {
@@ -7,7 +8,11 @@ namespace E_Ticaret.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-        public string Description { get; set; }
+        [DisplayName("Dislplay Order")]
+        [Range(1,100)]
+        public string DisplayOrder { get; set; }
     }
 }
